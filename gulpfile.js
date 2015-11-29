bebe691af66a7a240e71bc33f6f3f1ea2876ffe7
rudnev-dm <rudnev.web@gmail.com>
2015-11-30 00:14:11 +0300

    Исправил файл

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
 
 gulp.task("style", function() {
   return gulp.src("sass/style.{sass,scss}")
